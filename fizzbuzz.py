a0a5bc6531707706c9c723a1086c88af17324616
James Prior and Tim <jep200404@columbus.rr.com>
2017-12-28 19:48:11 -0500

    fizzbuzz.py: Refactor for DRY.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 def fizzbuzz(x):
-    if x % 5 == 0 and x % 3 == 0:
-        return 'FizzBuzz'
-    elif x % 3 == 0:
-        return 'Fizz'
-    elif x % 5 == 0:
-        return 'Buzz'
-    else:
-        return str(x)
+    words = ''
+    if x % 3 == 0:
+        words = 'Fizz'
+    if x % 5 == 0:
+        words += 'Buzz'
+    if words == '':
+        words = str(x)
+    return words
